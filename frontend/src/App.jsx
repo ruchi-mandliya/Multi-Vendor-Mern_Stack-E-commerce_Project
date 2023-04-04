d926f75e6af9f1301d58cab662272cf22d230f96
Ruchi Mandliya <rmandliya18@gmail.com>
2023-04-04 22:37:15 +0530

    add signup page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom";
-import {LoginPage} from './Routes.jsx';
+import {LoginPage,SignupPage} from './Routes.jsx';
 import './App.css';
 
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/login" element={<LoginPage/>}>
+      <Route path="/login" element={<LoginPage/>}/>
+      <Route path="/sign-up" element={<SignupPage/>}/>
 
-      </Route>
+     
     </Routes>
     </BrowserRouter>
    
